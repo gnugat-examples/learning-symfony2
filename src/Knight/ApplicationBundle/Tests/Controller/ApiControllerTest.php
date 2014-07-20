e15e3d0b69e55d2619f0f8ca1143f61f343d3a9c
Loïc Chardonnet <loic.chardonnet@gmail.com>
2014-07-20 12:53:27 +0200

    Added tests

---
+++ src/Knight/ApplicationBundle/Tests/Controller/ApiControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+// File: src/Knight/ApplicationBundle/Tests/Controller/ApiControllerTest.php
+
+namespace TempoSimple\Test\Functional\Web;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiControllerTest extends WebTestCase
+{
+    private function post($uri, array $data)
+    {
+        $content = json_encode($data);
+        $client = static::createClient();
+        $client->request('POST', $uri, array(), array(), array(), $content);
+
+        return $client->getResponse();
+    }
+
+    public function testOfferingTheRightThing()
+    {
+        $response = $this->post('/api/ni', array('offering' => 'shrubbery'));
+
+        $this->assertSame(Response::HTTP_OK , $response->getStatusCode());
+    }
+
+    public function testOfferingTheWrongThing()
+    {
+        $response = $this->post('/api/ni', array('offering' => 'hareng'));
+
+        $this->assertSame(Response::HTTP_UNPROCESSABLE_ENTITY , $response->getStatusCode());
+    }
+}
